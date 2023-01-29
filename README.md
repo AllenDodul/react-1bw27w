33419861293069c622e736fc173576a9e8630ac5
AllenDodul <109014717+AllenDodul@users.noreply.github.com>
2023-01-29 12:22:31 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-1bw27w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1bw27w)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+import TodoApp from './TodoApp'
+export default function App() {
+  return (
+    <div>
+      <h1>Todo App Chat gpt</h1>
+      <TodoApp />
+    </div>
+  );
+}
